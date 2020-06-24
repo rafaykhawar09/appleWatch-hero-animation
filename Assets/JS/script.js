89da09ebc193b3acae9caa47dee9d618ed9bbbd6
Rafay Khawar <rafaykhawar09@gmail.com>
2020-06-23 19:18:00 -0700

    pushing all files

---
+++ Assets/JS/script.js
@@ -0,0 +1,31 @@
+
+let screenHeight = 937;
+
+$(window).scroll(function(){
+
+     if( $(this).scrollTop() >= screenHeight/5 ){
+          $(".watchFaces-container-wrapper").addClass("night-bkg");
+          $(".watchFaces-container-wrapper").removeClass("day-bkg");
+     }
+     else{
+          $(".watchFaces-container-wrapper").addClass("day-bkg");
+          $(".watchFaces-container-wrapper").removeClass("night-bkg");
+     }
+
+
+     if( $(this).scrollTop() >= screenHeight){
+          $(".watchFaces-container-wrapper").addClass("pos-abs")
+          $(".watchFaces-container-wrapper").removeClass("pos-fixed")
+     }
+     else{
+          $(".watchFaces-container-wrapper").removeClass("pos-abs")
+          $(".watchFaces-container-wrapper").addClass("pos-fixed")
+     }
+
+     let angle = $(this).scrollTop()*18/100;
+
+     if(angle >= 0 && angle <= 108){
+
+          $(".watchFaces-container").css({"transform": `translate(-50%, -50%) rotateZ(${-$(this).scrollTop()*18/100}deg)`});
+     }
+});
